9101d017d910edf580e8a9a49d15337ca2b84fdc
r0zzy5 <rbosborn88@gmail.com>
2023-11-22 21:52:21 +0000

    Model forms now handled server side inside of client side

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, abort, request, url_for, redirect
 from pymongo import MongoClient
+from wtforms import Form, BooleanField, StringField, IntegerField, FloatField
 import glob
 import re
 import json
@@ -14,6 +15,24 @@ client = MongoClient('db', 27017, username='root', password='root')
 # use the "data_fabric" database
 db = client.data_fabric
 
+# create a lookup of form field classes for dynamic form creation later
+formFields = {'number':FloatField, 'string':StringField, 'integer':IntegerField, 'boolean':BooleanField}
+
+def generateModelForm(schema, data):
+    # create empty form
+    class ModelForm(Form):
+        pass
+    
+    # add all fields from schema
+    for key, val in schema.items():
+        fieldType = val.get('type')
+        if not fieldType:
+            abort(500)
+        setattr(ModelForm, key, formFields[fieldType](key,default=val.get('default')))
+    
+    return ModelForm(data)
+
+
 @app.route('/')
 def models():
     # get list of all json files in models folder
@@ -27,6 +46,7 @@ def models():
 
     return render_template('models.html', title="Models", models=models)
 
+
 @app.route('/model/<id>', methods=('GET', 'POST'))
 def model(id):
     # check if request is for a valid model, throw 404 if not
@@ -36,11 +56,22 @@ def model(id):
     
     # extract id and model name from filename
     matches = re.search(r'(\d{3})\-(\w+)\.json',files[0])
+
+    # read model schema
+    with open(files[0]) as f:
+        schema = json.loads(f.read()).get('schema')
     
-    # check if form has been submitted
-    if request.method == 'POST':
+    # error if no valid schema
+    if not schema:
+        abort(500)
+    
+    # generate a form based on model schema
+    form = generateModelForm(schema, request.form)
+    
+    # check if a valid form has been submitted
+    if request.method == 'POST' and form.validate():
         # generate json string from form contents
-        scen = json.dumps(request.form)
+        scen = json.dumps(form.data)
 
         # generate md5 hash of scenario json for uniqueness check
         cs = hashlib.md5(scen.encode("utf-8")).hexdigest()
@@ -55,7 +86,7 @@ def model(id):
         # if nothing is found, this is a new run so insert in to the database
         to_print = [scen, cs]
         if not check:
-            meta = {'hash':cs, 'scenario':request.form}
+            meta = {'hash':cs, 'scenario':form.data}
             collection.insert_one(meta)
             to_print = ['New Run'] + to_print
         else:
@@ -63,9 +94,5 @@ def model(id):
         
         return render_template('debug.html', to_print=to_print)
     
-    # for not submitted (GET request), so read model schema
-    with open(files[0]) as f:
-        form_json = f.read()
-    
     # display scenario entry form to user
-    return render_template('model.html', model=matches[2], form_json=form_json)
+    return render_template('model.html', model=matches[2], form=form)
